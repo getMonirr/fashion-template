1f2fea01d511e92aef16e1deabe2f63efffc1d50
Monir Hoshen <getmonirr@gmail.com>
2023-01-21 12:47:57 +0600

    header and nav added

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "penguin-color": "#FABE4C",
+        "penguin-bg": "#FFFBF0",
+        "penguin-para": "#363958",
+        "gradient-1": "#A4BC46",
+        "gradient-2": "#85A019"
+      },
+      lineHeight: {
+        "penguin-header": "96px"
+      }
+    },
+  },
+  plugins: [],
+}
